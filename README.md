1372cd7fa2dd9a53410955e329d79b732e6b6afe
Transnano <transnano.jp@gmail.com>
2020-07-09 05:52:52 +0000

    :construction: debug version.

--- README.md
+++ README.md
@@ -87,12 +87,13 @@ user_allow_other
 
 $ mkdir /tmp/mountpoint
 $ sudo ./hello /tmp/mountpoint &
-sudo ls -Fla /tmp/mountpoint &
-sudo cat /tmp/mountpoint/file.txt
+$ sudo ls -Fla /tmp/mountpoint
+# This file is read-only
+$ sudo cat /tmp/mountpoint/read-only-file.txt
 
 $ sudo ./hello /tmp/mountpoint &
-sudo echo google >> /tmp/mountpoint/file.txt
-bash: /tmp/mountpoint/file.txt: Permission denied
+$ sudo echo google >> /tmp/mountpoint/writable-file.txt
+bash: /tmp/mountpoint/writable-file.txt: Permission denied
 
 # 上記コマンド実行時に以下のエラーが出る場合は、fusermountコマンドを実行
 #/bin/fusermount: failed to access mountpoint /tmp/mountpoint: Transport endpoint is not connected
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ package main
 import (
 	"context"
 	"flag"
+	"os"
 	"sync"
 	"syscall"
 	"time"
@@ -20,12 +21,6 @@ import (
 
 type HelloRoot struct {
 	fs.Inode
-
-	// When file systems are mutable, all access must use
-	// synchronization.
-	mu      sync.Mutex
-	content []byte
-	mtime   time.Time
 }
 
 func (r *HelloRoot) OnAdd(ctx context.Context) {
@@ -43,23 +38,88 @@ func (r *HelloRoot) OnAdd(ctx context.Context) {
 				Mtimensec: uint32(time.Now().Nanosecond()),
 			},
 		}, fs.StableAttr{Ino: 2})
-	r.AddChild("file.txt", ch, false)
+	r.AddChild("read-only-file.txt", ch, false)
+
+	node := r.NewPersistentInode(
+		ctx, &bytesNode{},
+		fs.StableAttr{
+			Mode: syscall.S_IFREG,
+			// Make debug output readable.
+			Ino: 2,
+		})
+	r.AddChild("writable-file.txt", node, true)
+
 }
 
 func (r *HelloRoot) Getattr(ctx context.Context, fh fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
 	out.Mode = 0755
+	logrus.Debugf("Getattr: %v - %v\n", fh, r)
 	return 0
 }
 
 var _ = (fs.NodeGetattrer)((*HelloRoot)(nil))
 var _ = (fs.NodeOnAdder)((*HelloRoot)(nil))
 
-// Implement handleless read.
-var _ = (fs.NodeReader)((*HelloRoot)(nil))
+// bytesNode is a file that can be read and written
+type bytesNode struct {
+	fs.Inode
 
-func (bn *HelloRoot) Read(ctx context.Context, fh fs.FileHandle, dest []byte, off int64) (fuse.ReadResult, syscall.Errno) {
+	// When file systems are mutable, all access must use
+	// synchronization.
+	mu      sync.Mutex
+	content []byte
+	mtime   time.Time
+}
+
+// Implement GetAttr to provide size and mtime
+var _ = (fs.NodeGetattrer)((*bytesNode)(nil))
+
+func (bn *bytesNode) Getattr(ctx context.Context, fh fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
 	bn.mu.Lock()
 	defer bn.mu.Unlock()
+	logrus.Debugf("Getattr: %v - %v\n", fh, bn)
+	bn.getattr(out)
+	return 0
+}
+
+func (bn *bytesNode) getattr(out *fuse.AttrOut) {
+	out.Size = uint64(len(bn.content))
+	out.SetTimes(nil, &bn.mtime, nil)
+}
+
+func (bn *bytesNode) resize(sz uint64) {
+	if sz > uint64(cap(bn.content)) {
+		n := make([]byte, sz)
+		copy(n, bn.content)
+		bn.content = n
+	} else {
+		bn.content = bn.content[:sz]
+	}
+	bn.mtime = time.Now()
+}
+
+// Implement Setattr to support truncation
+var _ = (fs.NodeSetattrer)((*bytesNode)(nil))
+
+func (bn *bytesNode) Setattr(ctx context.Context, fh fs.FileHandle, in *fuse.SetAttrIn, out *fuse.AttrOut) syscall.Errno {
+	bn.mu.Lock()
+	defer bn.mu.Unlock()
+	logrus.Debugf("Setattr: %v - %v\n", fh, bn)
+
+	if sz, ok := in.GetSize(); ok {
+		bn.resize(sz)
+	}
+	bn.getattr(out)
+	return 0
+}
+
+// Implement handleless read.
+var _ = (fs.NodeReader)((*bytesNode)(nil))
+
+func (bn *bytesNode) Read(ctx context.Context, fh fs.FileHandle, dest []byte, off int64) (fuse.ReadResult, syscall.Errno) {
+	bn.mu.Lock()
+	defer bn.mu.Unlock()
+	logrus.Debugf("Read: %v - %v\n", fh, bn)
 
 	end := off + int64(len(dest))
 	if end > int64(len(bn.content)) {
@@ -72,20 +132,32 @@ func (bn *HelloRoot) Read(ctx context.Context, fh fs.FileHandle, dest []byte, of
 }
 
 // Implement handleless write.
-var _ = (fs.NodeWriter)((*HelloRoot)(nil))
+var _ = (fs.NodeWriter)((*bytesNode)(nil))
 
-func (bn *HelloRoot) Write(ctx context.Context, fh fs.FileHandle, buf []byte, off int64) (uint32, syscall.Errno) {
+func (bn *bytesNode) Write(ctx context.Context, fh fs.FileHandle, buf []byte, off int64) (uint32, syscall.Errno) {
 	sz := int64(len(buf))
 
-	logrus.Infof("%s :: %v ¥n", string(buf), off)
+	logrus.Infof("Write: %s :: %v\n", string(buf), off)
 	return uint32(sz), 0
 }
 
 // Implement (handleless) Open
-var _ = (fs.NodeOpener)((*HelloRoot)(nil))
+var _ = (fs.NodeOpener)((*bytesNode)(nil))
 
-func (f *HelloRoot) Open(ctx context.Context, openFlags uint32) (fh fs.FileHandle, fuseFlags uint32, errno syscall.Errno) {
+func (f *bytesNode) Open(ctx context.Context, openFlags uint32) (fh fs.FileHandle, fuseFlags uint32, errno syscall.Errno) {
 	return nil, 0, 0
+}
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	// logrus.SetFormatter(&logrus.JSONFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	logrus.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	logrus.SetLevel(logrus.DebugLevel)
 }
 
 func main() {
